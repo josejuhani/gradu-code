74ceab6b2a63a42ee9830d8868e0713c52ff099d
Jose Malmberg <jose.j.malmberg@student.jyu.fi>
2017-11-08 16:37:17 +0200

    Added comments to the code and fix the main-method

--- Boreal.py
+++ Boreal.py
@@ -12,6 +12,7 @@ class BorealSolver(object):
     def __init__(self, solver='glpk', tee=True):
         self.solver = solver
         self.tee = tee
+        self._solved = False
         self.res = None
         self.model = None
 
@@ -23,15 +24,19 @@ class BorealSolver(object):
         model.I = RangeSet(0, model.n-1)
         model.J = RangeSet(0, model.m-1)
 
+        # Initialize all x_ij = 0.0, when j != 0, and all xi0 = 1.0
         model.x = Var(model.I, model.J, domain=Binary, initialize=0.0)
         for i in model.I:
             model.x[i, 0].value = 1.0
 
+        # Initialize c_ij from given data
         def c_init(model, i, j):
             return data.values[i, j]
 
         model.c = Param(model.I, model.J, initialize=c_init)
 
+        '''Objective function: Formulate problem as binary problem.
+        Product x_ij*c_ij over all the i:s and j:s'''
         def obj_fun(model):
             return sum(sum(model.x[i, j]*model.c[i, j]
                            for i in model.I)
@@ -39,10 +44,12 @@ class BorealSolver(object):
 
         model.OBJ = Objective(rule=obj_fun, sense=maximize)
 
-        def constra(model, i):
+        ''' Constraint, so that every stand has only one handling
+        sum of x_ij for given i and  all j:s  equals 1'''
+        def const(model, i):
             return sum(model.x[i, j] for j in model.J) == 1
 
-        model.Constraint1 = Constraint(model.I, rule=constra)
+        model.Constraint1 = Constraint(model.I, rule=const)
 
         opt = SolverFactory(self.solver)
         self.res = opt.solve(model, tee=self.tee)
@@ -52,8 +59,9 @@ class BorealSolver(object):
 if __name__ == '__main__':
     data_dir = os.path.join(os.getcwd(), '../boreal_data')
     carbon = pd.read_csv(os.path.join(data_dir, 'Carbon_storage.csv'))
+    # Removes all lines containing Nan-values
     carbon_clean = carbon.dropna(axis=0, how='any')
-    data = carbon_clean[:100]
+    data = carbon_clean[:1000]
     solver = BorealSolver()
     solver.solveBoreal(data)
-    solver.x.display()
+    solver.model.x.display()
