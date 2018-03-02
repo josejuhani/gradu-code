2bd5d4c806d830cbb157cd9058173a2d2f6b620a
Jose Malmberg <jose.j.malmberg@student.jyu.fi>
2018-03-02 16:51:20 +0200

    Changed center assignign. New utility method.

--- generateclusters.py
+++ generateclusters.py
@@ -4,41 +4,74 @@ import numpy as np
 import pandas as pd
 import gradutil as gu
 import simplejson as json
+from scipy.spatial.distance import euclidean
+from time import time
 
 
-def clustering_to_optims(x_orig, x_opt, name, clustering, opt, logger=None,
-                         starttime=None):
+def clustering_to_optims(x_orig, x_clust, x_opt, names, clustering, opt,
+                         logger=None, starttime=None):
+    from time import time
     optims = dict()
     for nclust in sorted(clustering.keys()):
         n_optims = dict()
         for seedn in clustering[nclust].keys():
             xtoc = np.array(clustering[nclust][seedn]['xtoc'])
-            dist = np.array(clustering[nclust][seedn]['dist'])
             w = np.array([sum(xtoc == i)
-                          for i in range(eval(nclust))
+                          for i in range(nclust)
                           if sum(xtoc == i) > 0])
-            c_close = np.array([x_opt[np.argmin(dist[xtoc == i])]
-                                for i in range(eval(nclust))
-                                if len(dist[xtoc == i]) > 0])
-            prob = BorealWeightedProblem(c_close, weights=w)
-            res = opt.solve(prob.model)
-            n_optims[seedn] = gu.model_to_real_values(x_orig,
-                                                            prob.model,
-                                                            xtoc)
+            # Calculate the euclidian center of the cluster (mean)
+            # and then the point closest to that center according to
+            # euclidian distance, and then use the data format meant
+            # for optimization
+            c_close = np.array([
+                x_opt[min(np.array(range(len(xtoc)))[xtoc == i],
+                          key=lambda index:
+                          euclidean(x_clust[index],
+                                    np.mean(x_clust[xtoc == i],
+                                            axis=0)))]
+                for i in range(nclust) if sum(xtoc == i) > 0
+            ])
+            problems = [BorealWeightedProblem(c_close[:, :, i], weights=w)
+                        for i in range(np.shape(c_close)[-1])]
+            for p in problems:
+                opt.solve(p.model)
+            n_optims[seedn] = dict()
+            for ind, name in enumerate(names):
+                n_optims[seedn][name] = dict()
+                n_optims[seedn][name]['real'] = gu.model_to_real_values(
+                    x_orig[:, :, ind],
+                    problems[ind].model,
+                    xtoc)
+                n_optims[seedn][name]['surrogate'] = gu.cluster_to_value(
+                    x_orig[:, :, ind], gu.res_to_list(problems[ind].model), w)
             if logger:
                 logger.info('Optimized {} clusters. Seed {}'.format(nclust,
                                                                     seedn))
             if starttime:
-                logger.info('Since start {}s.'.format(time()-starttime))
+                logger.info('Since start {:2.0f} s.'.format(time()-starttime))
         optims[nclust] = n_optims
         if logger:
-            logger.info('Optimized {} clusters'.format(nclust))
+            logger.info('Optimized {} clusters with every seed'.format(nclust))
         if starttime:
-            logger.info('Since start {}s.'.format(time()-starttime))
-        with open('optimizations/dump{}{}.json'.format(name,
-                                                       nclust), 'w') as file:
+            logger.info('Since start {:2.0f}s.'.format(time()-starttime))
+        with open('optimizations/dump_all{}.json'.format(nclust), 'w') as file:
             json.dump(n_optims, file)
     return optims
+
+
+def clustering_to_dict(readfile):
+    with open(readfile, 'r') as rfile:
+        clustering = json.loads(rfile.read())
+
+    new_clustering = dict()
+    for nclust in clustering.keys():
+        new_clustering[eval(nclust)] = dict()
+        for seedn in clustering[nclust].keys():
+            new_clustering[eval(nclust)][eval(seedn)] = dict()
+            for key in clustering[nclust][seedn].keys():
+                new_clustering[eval(nclust)][eval(seedn)][key] = \
+                                    np.array(clustering[nclust][seedn][key])
+    return new_clustering
 
 
 def eval_dists(clustering, key):
@@ -65,12 +98,12 @@ def clustering(x, nclusts, seeds, logger=None, starttime=None):
                 logger.info('Clustered to {} clusters. Seed {}'.format(nclust,
                                                                        seedn))
             if starttime:
-                logger.info('Since start {}s.'.format(time()-starttime))
+                logger.info('Since start {:2.0f}s.'.format(time()-starttime))
         res[nclust] = res_clust
         if logger:
-            logger.info('Clustered to {} clusters'.format(nclust))
+            logger.info('Clustered to {:2.0f} clusters'.format(nclust))
         if starttime:
-            logger.info('Since start {}s.'.format(time()-starttime))
+            logger.info('Since start {:2.0f}s.'.format(time()-starttime))
         with open('clusterings/dump{}.json'.format(nclust), 'w') as file:
             json.dump(res_clust, file)
     return res
@@ -78,7 +111,6 @@ def clustering(x, nclusts, seeds, logger=None, starttime=None):
 
 if __name__ == '__main__':
     import logging
-    from time import time
     logging.basicConfig(level=logging.INFO)
     start = time()
     logger = logging.getLogger(__name__)
@@ -101,7 +133,7 @@ if __name__ == '__main__':
     x_norm_stack = gu.normalize(x_stack)
     nclusts1 = range(100, 8500, 200)
     seeds = range(2, 12)
-    logger.info('Initialized. {}'.format(time()-start))
+    logger.info('Initialized. Since start {:2.0f} sec'.format(time()-start))
     '''
     clustering1 = clustering(x_norm, nclusts1, seeds, logger, start)
     with open('clusterings/dump8500all.json', 'w') as file:
@@ -125,55 +157,23 @@ if __name__ == '__main__':
     logger.info('All clustered to 600 (second round). Time since start {}.'.
                 format(time()-start))
     '''
-    import os
-    dire = os.path.join(os.getcwd(), 'clusterings')
-    with open(os.path.join(dire, 'dump8500all.json'), 'r') as file:
-        clustering8500 = json.loads(file.read())
-    logger.info('Read clustering file.{}'.format(time()-start))
+    clustering_file = 'clusterings/dump8500all.json'
+    clustering_dict = clustering_to_dict(clustering_file)
+    logger.info('Read clustering file. Since start {:2.0f} sec'.
+                format(time()-start))
 
-    optims_revenue = clustering_to_optims(n_revenue.values,
-                                          revenue_norm,
-                                          'revenue',
-                                          clustering8500,
-                                          opt,
-                                          logger=logger,
-                                          starttime=start)
-    with open('optimizations/dumprevenue8500all.json', 'w') as file:
-        json.dump(optims_revenue, file)
-    logger.info('Revenue optimized. {}'.format(time()-start))
-
-    optims_carbon = clustering_to_optims(n_carbon.values,
-                                         carbon_norm,
-                                         'carbon',
-                                         clustering8500,
-                                         opt,
-                                         logger=logger,
-                                         starttime=start)
-    with open('optimizations/dumpcarbon8500all.json', 'w') as file:
-        json.dump(optims_carbon, file)
-    logger.info('Carbon optimized. {}'.format(time()-start))
-
-    optims_deadwood = clustering_to_optims(n_deadwood.values,
-                                           deadwood_norm,
-                                           'deadwood',
-                                           clustering8500,
-                                           opt,
-                                           logger=logger,
-                                           starttime=start)
-    with open('optimizations/dumpdeadwood8500all.json', 'w') as file:
-        json.dump(optims_deadwood, file)
-    logger.info('Deadwood optimized. {}'.format(time()-start))
-
-    optims_ha = clustering_to_optims(n_ha.values,
-                                     ha_norm,
-                                     'ha',
-                                     clustering8500,
-                                     opt,
-                                     logger=logger,
-                                     starttime=start)
-    with open('optimizations/dumpha8500all.json', 'w') as file:
-        json.dump(optims_ha, file)
-    logger.info('HA optimized. {}'.format(time()-start))
+    names = ['revenue', 'carbon', 'deadwood', 'ha']
+    optims = clustering_to_optims(x_stack,
+                                  x_norm,
+                                  x_norm_stack,
+                                  names,
+                                  clustering_dict,
+                                  opt,
+                                  logger=logger,
+                                  starttime=start)
+    with open('optimizations/dumpall8300all.json', 'w') as file:
+        json.dump(optims, file)
+    logger.info('All optimized. Since start {:2.0f} sec'.format(time()-start))
 
     nclusts4 = range(9900, 20000, 200)
     logger.info('Still some more super extra. {}'.format(time()-start))
